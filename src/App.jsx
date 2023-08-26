9c1963745c61b91e5a66e1760c7555321518be64
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-26 21:23:29 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import './App.css'
+import './test.css'
+
+function App() {
+  var number = 10 
+
+
+  return (
+   
+     <div>
+      <h1>{1+1+10}</h1>
+      <h3>{number}</h3>
+      <h1>bye world</h1>
+     <p>Hello world</p>
+     <div>
+      <h2 style={{color:'yellow'}}>hello</h2>
+     </div>
+     </div>
+   
+  )
+}
+
+export default App
