0b0f9e0f6ef6b27b5ff10d1bcc55033d9d346974
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-29 20:30:51 +0545

    learn useEffect

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,36 @@
 
-import Button from '../components/Button'
-import Navbar from '../components/Navbar'
+
+import { useEffect, useState } from 'react'
 import './Contact.css'
 
 const Contact = () => {
+  const [number,setNumber] = useState(0)
+  const [number2,setNumber2] = useState(0)
+
+  // jati khera ni render huda run hunchha , kehi ko state change huda run hunchha(1st)
+  // useEffect(()=>{
+  //   console.log("Hello")
+
+  // })
+
+  // first page mount huda yo vitra ko code run hunchha (2nd)
+  useEffect(()=>{
+    console.log("Hello")
+
+  },[])
+
+  // jaba tyo stateName ko state change/render hunchha (3rd)
+  // useEffect(()=>{
+  //   console.log("Hello")
+    
+
+  // },[number])
   return (
     <div>
-       
-        <Button text="Contact" />
-        <h1>Contact Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quisquam laboriosam dolore ipsa odio necessitatibus explicabo, vero voluptate sequi rerum maxime doloribus consequatur vel omnis eligendi praesentium dolorum labore minus!
-        Placeat dicta nam dolorem laboriosam ducimus aliquam fuga commodi sit architecto corrupti repellat mollitia eius corporis nulla, cum quibusdam. Ipsum iste error molestias in dolore nisi dolores voluptatibus similique nostrum.
-        Magnam neque, eos deserunt quidem corrupti tenetur porro impedit ducimus vero nesciunt quasi modi molestias, magni omnis nobis autem atque accusantium culpa voluptatibus blanditiis animi! Reprehenderit quae consequatur sit ducimus!
-        Alias, dicta nesciunt aut qui adipisci, quisquam expedita porro consectetur incidunt tempora deleniti et eius ea molestiae natus. Quos sit pariatur, porro aut voluptate excepturi doloribus molestiae maiores impedit nostrum?</p>
-        <Navbar />
+       <h1>{number}</h1>
+        <button onClick={()=>setNumber(number+1)}>Increase</button>
+        <h1>{number2}</h1>
+        <button onClick={()=>setNumber2(number2-1)}>Decrease</button>
     </div>
   )
 }
